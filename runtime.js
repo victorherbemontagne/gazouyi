5d869e2a48639b35600fafdfda38eb05e4df7e23
Victor Dh <victordherbemont@gmail.com>
2020-11-30 12:23:45 +0100

    New version of the website

--- runtime.js
+++ runtime.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "" + ({"home-home-module":"home-home-module","pages-pages-module":"pages-pages-module"}[chunkId]||chunkId) + ".js"
+/******/ 		return __webpack_require__.p + "" + ({"pages-pages-module":"pages-pages-module"}[chunkId]||chunkId) + ".js"
 /******/ 	}
 /******/
 /******/ 	// The require function
