97d85f8e1a6bf17cd4c250070ef85f6c5221e26e
Victor Dh <victordherbemont@gmail.com>
2021-01-07 11:07:05 +0100

    New version of the site

--- polyfills.js
+++ polyfills.js
@@ -10539,8 +10539,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\DELL\Desktop\Gazouyi\gizouyi-site\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\DELL\Desktop\Gazouyi\gizouyi-site\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\DELL\Desktop\Gazouyi\site-vitrine\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\DELL\Desktop\Gazouyi\site-vitrine\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
